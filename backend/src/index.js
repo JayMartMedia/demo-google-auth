b9cae35bf680d516e14fc2cd51d42076e17275c5
JayMartMedia <Jay@JayMartMedia.com>
2022-06-11 02:25:25 -0400

    feat: add frontend button to call secure endpoint, add express server with secure endpoint which validates jwt sent in request

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,51 @@
-console.log('index called');
+// setup express
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors({
+  origin: [
+    "http://localhost:4400"
+  ]
+}));
+const port = 4401
+
+// configure google auth
+const { OAuth2Client } = require("google-auth-library");
+const CLIENT_ID = "403706356522-9l5kmo3oujjk8ho182ec3kts8k96d935.apps.googleusercontent.com";
+const client = new OAuth2Client(CLIENT_ID);
+
+async function verify(token) {
+  token = token.replace("Bearer ", "");
+  try {
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: CLIENT_ID
+    });
+    console.log("ticket", ticket);
+    return true;
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
+}
+
+// setup endpoints
+app.get("/helloworld", (req, res) => {
+  res.send("Hello World!")
+})
+
+app.get("/secure", async (req, res) => {
+  const authHeader = req.headers.authorization;
+  console.log("Auth Header: ", authHeader);
+  // TODO: convert to middleware
+  if (await verify(authHeader)) {
+    res.send({ message: "Here is secure response!" });
+  } else {
+    res.sendStatus(401).send();
+  }
+})
+
+// start server
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
--- frontend/www/main.js
+++ frontend/www/main.js
@@ -1,1 +1,23 @@
-console.log('main loaded');
+// add onclick listener to button
+window.addEventListener("load", () => {
+  const btn = document.querySelectorAll("#send_secure_request_btn").item(0);
+  btn.addEventListener("click", () => {
+    handleSendSecureRequestBtnClick();
+  })
+})
+
+// called when send_secure_request_btn is clicked
+function handleSendSecureRequestBtnClick() {
+  sendSecureRequest();
+}
+
+// use fetch to send request, and log response data
+async function sendSecureRequest() {
+  const apiHost = "http://localhost:4401";
+  const res = await fetch(`${apiHost}/secure`, {
+    headers: new Headers({
+      "Authorization": window.jwt
+    }),
+  });
+  console.log("response data", await res.json());
+}
